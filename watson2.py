eafdcfcf8cf1ad3cff0f66f3f962d87e618d8840
Abhishek Venkatesh <abhi.iyer42@gmail.com>
2018-08-28 14:36:54 +0530

    removed tokens

--- watson2.py
+++ watson2.py
@@ -21,13 +21,13 @@ def message(bot, update):
     print('Received an update')
     global context
 
-    conversation = ConversationV1(username='6f64e7f6-b44c-4b9d-b379-161a45939e6a',  # TODO
-                                  password='bPjwhQxzBUvG',  # TODO
+    conversation = ConversationV1(username='usernamehere',  # TODO
+                                  password='passwordhere',  # TODO
                                   version='2018-02-16')
 
     # get response from watson
     response = conversation.message(
-        workspace_id='2bc9c395-797c-4996-85ce-91af808412d7',  # TODO
+        workspace_id='workspace_idhere',  # TODO
         input={'text': update.message.text},
         context=context)
     print(json.dumps(response, indent=2))
@@ -42,7 +42,7 @@ def message(bot, update):
 
 def main():
     # Create the Updater and pass it your bot's token.
-    updater = Updater('590080454:AAE02s747BI7Db3wVe6w6aaFg-YKEEGdol8')  # TODO
+    updater = Updater('your token here')  # TODO
 
     # Get the dispatcher to register handlers
     dp = updater.dispatcher
